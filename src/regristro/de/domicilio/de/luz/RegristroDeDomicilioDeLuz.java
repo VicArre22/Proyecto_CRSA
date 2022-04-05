d5886e7dc5c0666d8465b77f3d6c1435feda54f9
CarlosCamargoAvila <fernandocama23@gmail.com>
2022-04-05 12:17:30 -0500

    FrmRegistro_de_Domicilio Carlos

---
+++ src/regristro/de/domicilio/de/luz/RegristroDeDomicilioDeLuz.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regristro.de.domicilio.de.luz;
+
+/**
+ *
+ * @author pacoa
+ */
+public class RegristroDeDomicilioDeLuz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
