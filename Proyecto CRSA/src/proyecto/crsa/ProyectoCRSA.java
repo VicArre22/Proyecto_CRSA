f1303c6eddcf56eedc1ff9adb170e19274be7b74
MelanieB21 <Castbellm@micorreo.upp.edu.mx>
2022-04-07 20:06:27 -0500

    Controlador Creado.java

---
+++ Proyecto CRSA/src/proyecto/crsa/ProyectoCRSA.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto.crsa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Cesar Bello
+ */
+public class ProyectoCRSA {
+
+       public static void main(String[] args) {
+		int i,j;
+		MDB administradorBD =  new MDB();
+		List<ArrayList<String>> datosObtenidos= new ArrayList<ArrayList<String>>();
+		ArrayList<String> renglonObtenido = new ArrayList<String>();
+		String leyenda;
+                //insertar
+                leyenda= administradorBD.registrarAlta("tbusuarios", "null, 'Pedro','usuarioMartínez',1234");
+		System.out.println(leyenda);
+                
+                //modificar
+	//leyenda = administradorBD.modificarRegistro("TbEquipo", "nombre='lapiz' , descripcion = 'digital' ,  cantidad= '4'", "id = 5");
+      //     System.out.println(leyenda);
+                
+                //Borrar
+         //  leyenda = administradorBD.borrarRegistro("TbUsuarios", "id = 3");
+           //   System.out.println(leyenda);
+		
+                //consulta
+		/*datosObtenidos = administradorBD.consultarDatos("tbUsuarios", "id , nombre,apPaterno, psw", "id=1");
+                  System.out.println(leyenda);
+		for(i=0;i<datosObtenidos.size();i++)
+		{
+                    renglonObtenido = datosObtenidos.get(i);
+                    for(j=0;j<renglonObtenido.size();j++){
+			System.out.print(renglonObtenido.get(j) + " ");
+                    }
+                    System.out.println();
+		}*/
+        }
+}
+
+    
+    
+
